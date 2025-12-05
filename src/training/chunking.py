18bf28434dfd0018b090581cb56e5e17a344353d
Alexander Esser <mail@alexander-esser.com>
2025-12-05 11:05:08 +0100

    Added pack_by_token_budget() for only tokens
    Token packing function for only tokens, without labels

--- src/training/chunking.py
+++ src/training/chunking.py
@@ -42,3 +42,35 @@ def pack_by_token_budget(tokenizer, tokens, labels, max_length):
         i = j
 
     return chunks_t, chunks_l
+
+
+def pack_by_token_budget_tokens(tokenizer, tokens, max_length):
+    """
+    Packs tokens into chunks that fit within the token budget of max_length after tokenization.
+
+    :param tokenizer: The tokenizer
+    :param tokens: Tokens
+    :param max_length: Maximum token length
+    :return: (tok_chunks, lab_chunks) after packing
+    """
+    chunks_t = []
+    i = 0
+    while i < len(tokens):
+        j = i + 1
+        while j <= len(tokens):
+            enc = tokenizer([tokens[i:j]], is_split_into_words=True, add_special_tokens=True, truncation=False, padding=False)
+            if len(enc["input_ids"][0]) <= max_length:
+                j += 1
+            else:
+                break
+
+        if j > len(tokens):
+            j = len(tokens)
+
+        if j == i:
+            j = i + 1
+
+        chunks_t.append(tokens[i:j])
+        i = j
+
+    return chunks_t
